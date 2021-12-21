d59594a66351c163bbb4b8a0d466a243e511c9d7
iloneslife <jasony1@naver.com>
2021-12-21 20:11:08 +0900

    UI 개발

--- lib/ui/detail_screen.dart
+++ lib/ui/detail_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:movie/data/movie_api.dart';
 
 class DetailScreen extends StatelessWidget {
   final String title;
@@ -23,18 +22,105 @@ class DetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final _api = MovieApi();
-
     return Scaffold(
         appBar: AppBar(
-          title: Text(''),
+          title: Text(title),
         ),
-        body: Column(
-          children: [
-            Text(''),
-            Row(),
-            Text(''),
-          ],
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Padding(
+            padding: const EdgeInsets.only(top: 16, bottom: 16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  style: const TextStyle(
+                      fontSize: 30, fontWeight: FontWeight.bold),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 16, bottom: 16),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        width: 200,
+                        height: 300,
+                        child: Image.network(
+                          'https://image.tmdb.org/t/p/original' +
+                              backdropPath.toString(),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              '개봉일 ' + releaseDate,
+                              style: const TextStyle(fontSize: 12),
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            Row(
+                              children: [
+                                SizedBox(
+                                  width: 65,
+                                  child: TextButton.icon(
+                                    onPressed: () {},
+                                    icon: const Icon(
+                                      Icons.check,
+                                      size: 12.0,
+                                      color: Colors.white,
+                                    ),
+                                    label: Text(
+                                      voteCount.toString(),
+                                      style: const TextStyle(
+                                          color: Colors.white, fontSize: 10),
+                                    ),
+                                    style: TextButton.styleFrom(
+                                        backgroundColor: Colors.blue),
+                                  ),
+                                ),
+                                const SizedBox(
+                                  width: 10,
+                                ),
+                                SizedBox(
+                                  width: 65,
+                                  child: TextButton.icon(
+                                    onPressed: () {},
+                                    icon: const Icon(
+                                      Icons.star,
+                                      size: 12.0,
+                                      color: Colors.white,
+                                    ),
+                                    label: Text(
+                                      voteAverage.toString(),
+                                      style: const TextStyle(
+                                          color: Colors.white, fontSize: 10),
+                                    ),
+                                    style: TextButton.styleFrom(
+                                        backgroundColor: Colors.blue),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Text(
+                  overView,
+                  maxLines: 6,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ],
+            ),
+          ),
         ));
   }
 }
